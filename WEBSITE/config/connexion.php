36b7621f762a37f07cfddda1d5653a802ea5c4a4
Hugo <hugo@opencertif.fr>
2025-04-29 14:41:08 +0200

    continuation

---
+++ WEBSITE/config/connexion.php
@@ -0,0 +1,29 @@
+
+<?php
+
+class Connexion{
+
+    static private $hostname = 'localhost'; 
+    static private $database = 'platformasso'; 
+    static private $login = '';
+    static private $mdp = ''; 
+
+    static private $tabUTF8 = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+    static private $pdo;
+    static public function pdo() {return self::$pdo;}
+
+    static public function connect(){
+        $h = self::$hostname; $d = self::$database;
+        $l = self::$login ; $m = self::$mdp;
+        $t = self::$tabUTF8; 
+
+        try{
+            self::$pdo = new PDO("mysql:host=$h;dbname=$d",$l,$m,$t);
+            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            echo "erreur de connexion : ".$e->getMessage()."<br>";
+        }
+    }    
+}
+
+?>
